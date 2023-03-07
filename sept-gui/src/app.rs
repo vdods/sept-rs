1bd0defa311fa54338a3f5f510ff3ae03f965ce3
Victor Dods <victor.dods@gmail.com>
2023-03-06 23:34:03 -0800

    A half-hearted attempt at getting wasm build working.

--- sept-gui/src/app.rs
+++ sept-gui/src/app.rs
@@ -153,16 +153,14 @@ impl eframe::App for App {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        // let Self { label, value } = self;
-
-        // #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
-                        frame.close();
+                        _frame.close();
                     }
                 });
             });
